210e967f7647aed54b4b5d5479301e00145cc8b8
Aaron Rogers <aaron@rocketmade.com>
2012-04-11 11:57:11 -0600

    Initial Commit

---
+++ ToDo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDo
+//
+//  Created by Aaron Rogers on 4/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
